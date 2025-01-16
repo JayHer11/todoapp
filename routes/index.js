584cef1c961743ff46c16d8203c4db46283fc1a0
x2p7 <x2p7@fresnou.org>
2025-01-16 09:00:20 -0800

    initial

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+const todoController = require("../controller/todo_controller");
+const {sequelize} = require("../models");
+const {QueryTypes} = require("sequelize");
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index');
-});
+router.get('/', todoController.homeRoute);
 
 module.exports = router;
+router.get('/:id', todoController.renderAddForm);
+router.post('/add', todoController.addNewItem);
+
+router.get('/complete/:id', todoController.markItemAsComplete)
+
+router.get('/incomplete/:id', todoController.markItemAsIncomplete)
+
+router.get('/delete/:id', todoController.deleteItem)
+
+router.get('/edit/:id', todoController.renderEditForm)
+router.post('/edit/:id', todoController.EditItem)
